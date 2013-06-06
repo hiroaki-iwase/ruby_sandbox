cede7308ced902734aaa61bcc18ecd7d654bbd74
hiroaki-iwase <hiroaki.iwase.r@gmail.com>
2013-06-06 19:33:57 +0900

    change class name and message action

--- hash.rb
+++ hash.rb
@@ -1,5 +1,4 @@
 # -*- encoding: utf-8 -*-
-#@name = Array.new(12){ |i| Array.new }
 
 class Pair
   attr_accessor :fst, :snd
@@ -9,8 +8,8 @@ class Pair
   end
 end
 
-
-class Name
+###change name
+class Hash
   def initialize
     @name = Array.new(12){ |i| Array.new }
   end
@@ -25,48 +24,54 @@ class Name
     (sum % 12)
   end
 
-  def set(family, first)
-    if @name[hash(family)].assoc family
-      puts "this family name are already used"
-      return
-    end
-
-    @name[hash(family)].push(Pair.new(family, first))
-    puts "STORED(#{@name})"
-  end
-
-  def get(family)
-    @name[hash(family)].each {|value|
-      if value.fst == family
-        puts "First name is #{value.snd}"
-        return
-      end
+#key valueに変更
+  def set(key, value)
+    @name[hash(key)].each {|value|
+      return false if value.fst == key
     }
-    puts "NOT STORED"
+    @name[hash(key)].push(Pair.new(key, value))
+    puts "#{@name}"
+    return true
   end
-end #end of Name class
 
-name = Name.new
+  def get(key)
+    @name[hash(key)].each {|value|
+      return value.snd if value.fst == key
+    }
+    return false
+  end
+end #end of Hash class
+ 
+name = Hash.new
 
 
 for i in 1..100
   puts "**************************************"
   puts "\r\n[1]input\r\n[2]output\r\n[3]finish"
   case gets.chomp!
-    when "1"
+
+    when "1" #SET
       print "input family name\r\n>"
       family_name = gets.chomp!
       print "input first name\r\n>"
       first_name = gets.chomp!
-      name.set(family_name, first_name)
+      if name.set(family_name, first_name)
+        puts "STORED"
+      elsif
+        puts "this family name are already used"
+      end
       next
 
-    when "2"
+    when "2" #GET
       print "input family name\r\n>"
-      name.get(gets.chomp!)
+      if f_name = name.get(gets.chomp!)
+        puts "First name is #{f_name}"
+      elsif
+        puts "NOT STORED\r\n"
+      end
       next
 
-    when "3"
+    when "3" #FINISH
       puts "finish"
       break
 
